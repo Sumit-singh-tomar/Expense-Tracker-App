285bedc610c4b41876f739e6cfb132445cf181f9
sumit singh tomar <sumitsinght10@gmail.com>
2024-01-19 19:25:20 +0530

    commit

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
+var j = 0
 function addExpense(event) {
     event.preventDefault()
     let expenseDetail = {
         expenseName: event.target.expenseName.value,
         expenseAmt: event.target.expenseAmt.value,
         date: event.target.date.value,
+    };
+
+    localStorage.setItem(event.target.expenseName.value, JSON.stringify(expenseDetail));
+
+
+    if (j == 0) {
+        var div = document.getElementById('show-table')
+        div.innerHTML = `
+    <table class="table">
+        <tr>
+            <th>Expense Name</th>
+            <th>Expense Amount</th>
+            <th>Date</th>
+            <th>Action</th>
+        </tr>
+    </table>`
+    j++
     }
-    localStorage.setItem(event.target.expenseName.value, JSON.stringify(expenseDetail))
 
-    var div = document.getElementById('show-table')
 
-    var table = document.createElement('table')
-    table.className = "table"
-    table.innerHTML = `
-    <tr>
-        <th>Expense Name</th>
-        <th>Expense Amount</th>
-        <th>Date</th></td>
-        <th>Action</th>
-    </tr>
+    var table = document.getElementsByClassName('table')[0];
 
-    <tr>
-        <td>${expenseDetail.expenseName}</td>
-        <td>&#8377;${expenseDetail.expenseAmt}.00</td>
-        <td>${expenseDetail.date}</td>
-        <td><button>Edit</button> <button>Delete</button></td>
-    </tr>`
+    var newRow = table.insertRow();
 
-    div.appendChild(table)
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+    var cell4 = newRow.insertCell(3);
+
+    cell1.innerHTML = expenseDetail.expenseName;
+    cell2.innerHTML = expenseDetail.expenseAmt;
+    cell3.innerHTML = expenseDetail.date;
+    cell4.innerHTML = `<button>Edit</button> <button>Delete</button>`;
 }
